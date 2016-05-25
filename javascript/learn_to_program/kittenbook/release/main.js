03ed7dd267a0050e94c970f9f1765bea8e1783b0
shoo7830 <shoo7830@gmail.com>
2016-05-26 02:09:58 +0900

    각종 테스트

---
+++ javascript/learn_to_program/kittenbook/release/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by shoo7830 on 2016. 5. 26..
+ */
+var projectName = 'kittenbook';
+var versionNumber = '0.0.1';
+var currentDate = new Date();
+
+var currentTime = currentDate.getFullYear() + '-' +
+    (currentDate.getMonth() + 1) + '-' +
+        currentDate.getDate() + ' at ' +
+        currentDate.getHours() + ':' +
+        currentDate.getMinutes() + ':' +
+        currentDate.getSeconds();
+/**
+ * Created by shoo7830 on 2016. 5. 25..
+ */
+var userName = prompt('Hello, what\'s your name?');
+document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' +
+'<p>' + projectName + ' ' + versionNumber + ' accessed on : ' + currentTime + '</p>';
