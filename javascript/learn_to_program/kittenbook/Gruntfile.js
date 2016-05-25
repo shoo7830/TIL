83fecdc0ab7c52e306ef09051c9ffab905c9aabc
shoo7830 <shoo7830@gmail.com>
2016-05-26 08:55:11 +0900

    grunt 설정 추가

--- javascript/learn_to_program/kittenbook/Gruntfile.js
+++ javascript/learn_to_program/kittenbook/Gruntfile.js
@@ -8,17 +8,21 @@ module.exports = function(grunt) {
     },
     copy: {
       release: {
-        src:'mainfest.json',
-        dest:'release/mainfest.json'
+        src:'manifest.json',
+        dest:'release/manifest.json'
       }
     },
     jshint: {
       files:['js/values.js','js/prompt.js']
+    },
+    watch: {
+      files:['<%=jshint.files%>', 'manifest.json'],
+      tasks:['default']
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-
-  grunt.registerTask('default', ['jshint', 'concat', 'copy']);
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['jshint', 'concat', 'copy']);
 };
