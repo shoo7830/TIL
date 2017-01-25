0e5da3486307d3839e1931d2d89fbdc554a38d0c
Aria Kim <shoo7830@gmail.com>
2017-01-25 11:15:45 +0900

    Update gulpfile.js

--- to-learn-in-2017/gulp/gulpfile.js
+++ to-learn-in-2017/gulp/gulpfile.js
@@ -1,34 +1,34 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    scssLint = require('gulp-scss-lint'),
-    stylish  = require('gulp-scss-lint-stylish2'),
-    autoprefixer = require('gulp-autoprefixer'),
-    plumber = require('gulp-plumber'),
-    sourcemaps = require('gulp-sourcemaps');
+	sass = require('gulp-sass'),
+	scssLint = require('gulp-scss-lint'),
+	stylish  = require('gulp-scss-lint-stylish2'),
+	autoprefixer = require('gulp-autoprefixer'),
+	plumber = require('gulp-plumber'),
+	sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
   return gulp.src('static/ui/css/**/*.scss')
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('static/ui/css'));
+	.pipe(plumber())
+	.pipe(sourcemaps.init())
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(sourcemaps.write('./maps'))
+	.pipe(gulp.dest('static/ui/css'));
 });
 
 gulp.task('scss-lint', function() {
-    var reporter = stylish();
+	var reporter = stylish();
 
-    gulp.src('static/ui/css/**/*.scss')
-        .pipe(plumber())
-        .pipe( scssLint({ customReport: reporter.issues }) )
-        .pipe( reporter.printSummary );
+	gulp.src('static/ui/css/**/*.scss')
+		.pipe(plumber())
+		.pipe( scssLint({ customReport: reporter.issues }) )
+		.pipe( reporter.printSummary );
 });
 
 gulp.task('watch', function () {
-    gulp.watch('static/ui/css/**/*.scss', ['sass', 'scss-lint']);
+	gulp.watch('static/ui/css/**/*.scss', ['sass', 'scss-lint']);
 });
 
 gulp.task('default', ['watch']);
