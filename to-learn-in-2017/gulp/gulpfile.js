5c541b83ec35e2ceb08c279e0555b89d874bd66f
shoo7830 <shoo7830@gmail.com>
2017-08-24 13:32:20 +0900

    gulpfile.js 수정 부분 반영

--- to-learn-in-2017/gulp/gulpfile.js
+++ to-learn-in-2017/gulp/gulpfile.js
@@ -1,34 +1,46 @@
 'use strict';
 
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	scssLint = require('gulp-scss-lint'),
-	stylish  = require('gulp-scss-lint-stylish2'),
-	autoprefixer = require('gulp-autoprefixer'),
-	plumber = require('gulp-plumber'),
-	sourcemaps = require('gulp-sourcemaps');
+    sass = require('gulp-sass'),
+    scssLint = require('gulp-scss-lint'),
+    stylish = require('gulp-scss-lint-stylish2'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber'),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sass', function () {
-  return gulp.src('static/ui/css/**/*.scss')
-	.pipe(plumber())
-	.pipe(sourcemaps.init())
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(autoprefixer())
-	.pipe(sourcemaps.write('./maps'))
-	.pipe(gulp.dest('static/ui/css'));
+gulp.task('sass', function() {
+    return gulp.src('static/ui/css/**/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('static/ui/css'));
 });
 
 gulp.task('scss-lint', function() {
-	var reporter = stylish();
+    var reporter = stylish();
 
-	gulp.src('static/ui/css/**/*.scss')
-		.pipe(plumber())
-		.pipe( scssLint({ customReport: reporter.issues }) )
-		.pipe( reporter.printSummary );
+    gulp.src('static/ui/css/**/*.scss')
+        .pipe(plumber())
+        .pipe(scssLint({ customReport: reporter.issues }))
+        .pipe(reporter.printSummary);
 });
 
-gulp.task('watch', function () {
-	gulp.watch('static/ui/css/**/*.scss', ['sass', 'scss-lint']);
+gulp.task('less', function() {
+    return gulp.src('static/ui/css/style.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('static/ui/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('static/ui/css/**/*.scss', ['sass', 'scss-lint']);
+    // gulp.watch('static/ui/css/style.less', ['less']);
 });
 
 gulp.task('default', ['watch']);
